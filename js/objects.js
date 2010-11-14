92c52769e31ef40d03564a66ab49f5204ab2bb2a
Glenn Y. Rolland <glenux@gmail.com>
2010-11-14 17:55:51 +0100

    objects.js: Reduced the number of balls.

--- js/objects.js
+++ js/objects.js
@@ -1,21 +1,12 @@
 
 
-/*
- function cleanContext( func ){
- return function(){
- tx.saveContext();
- func();
- ctx.restoreContext();
-	   }
-       }*/
-
 window.onload = function(){
     var world = new World();
     var map = new Map( world, "map");
     var hole = new Hole( world, map.size / 2, map.size / 2 );
     var ball = new Ball( world );
     var racket;
-    for (var i=0; i<10; i++) {
+    for (var i=0; i<5; i++) {
 	racket = new Racket( world );
     }
     var interval = setInterval( function() { world.tick(); }, 40 );
