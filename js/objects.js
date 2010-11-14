d4b4e46bae72d449d20ab687c6174b5d474ac62c
Glenn Y. Rolland <glenux@gmail.com>
2010-11-14 17:17:05 +0100

    Added a lot of rackets (to test collisions).

--- js/objects.js
+++ js/objects.js
@@ -14,8 +14,10 @@ window.onload = function(){
     var map = new Map( world, "map");
     var hole = new Hole( world, map.size / 2, map.size / 2 );
     var ball = new Ball( world );
-    var racket1 = new Racket( world );
-    var racket2 = new Racket( world );
+    var racket;
+    for (var i=0; i<10; i++) {
+	racket = new Racket( world );
+    }
     var interval = setInterval( function() { world.tick(); }, 40 );
     //var interval = setTimeout( function() { world.tick(); }, 40 );
     document.onkeydown = function(e){ world.keydown(e); };
