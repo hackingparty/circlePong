224bd9818e29da39c0af5d11766a8fa0d94d54a3
Glenn Y. Rolland <glenux@gmail.com>
2010-11-14 17:18:47 +0100

    Added collision detection for rackets.

--- js/ball.js
+++ js/ball.js
@@ -6,14 +6,15 @@ function Ball( world, x, y ) {
     this.DEFAULT_SPEED = 7;
     this.DEFAULT_SIZE = 5;
     this.DEFAULT_DIRECTION = 0;
+    this.COLLISION_EPSILON = 1/1000;
 
     // methods
     this.update = function() {
-	var dx = Math.cos( this.direction ) * this.speed;
-	var dy = Math.sin( this.direction ) * this.speed;
+	this.dx = Math.cos( this.direction ) * this.speed;
+	this.dy = Math.sin( this.direction ) * this.speed;
 
-	this.x += dx;
-	this.y += dy;
+	this.x += this.dx;
+	this.y += this.dy;
     };
 
     this.draw = function(){
@@ -31,26 +32,68 @@ function Ball( world, x, y ) {
 	ctx.restore();
     };
 
-    this.collide = function( candidate ) {
-	// collision with the map
-	// => distance from the center is greater than map ray
+    this.collide = function() {
 	var map_center = this.world.map.size / 2;
 	var map_ray = this.world.map.ray;
-	var dist = Math.sqrt( (map_center - this.x) * (map_center - this.x) 
+	var dist_to_center = Math.sqrt( (map_center - this.x) * (map_center - this.x) 
 	    + (map_center - this.y) * (map_center - this.y) );
-	if ( dist > map_ray ) {
+
+	// test collision with rackets
+	for (var ridx = 0; ridx < this.world.rackets.length; ridx++){
+	    var racket = this.world.rackets[ridx];
+	    var dist_to_racket = 0; 
+
+	    // previous ball position
+	    var prev = { 
+		x : this.x - this.dx , 
+		y : this.y - this.dy 
+	    };
+
+	    // intersection  is
+	    //   inter = prev + u1 ( this - prev )
+	    // and also
+	    //   inter = racket.start + u2 ( racket.stop - racket.start )
+	    
+	    // means that
+	    //   prev + u1 ( this - prev ) = racket.start + u2 ( racket.stop - racket.start )
+	    
+	    // solving that :
+	    var det = racket.dy * this.dx - racket.dx * this.dy;
+	    
+	    // if parallels, skip this racket
+	    if ( det < this.COLLISION_EPSILON ) {
+		// console.log("skip racket ( det ) " + ridx);
+		continue;
+	    }
+	    //console.log("racket " + ridx + " det = " + det);
+
+	    var u1 = ( racket.dx * ( prev.y - racket.start.y ) - racket.dy * ( prev.x - racket.start.x ) ) / det;
+	    var u2 = ( this.dx * ( prev.y - racket.start.y ) - this.dy * ( prev.x - racket.start.x ) ) / det;
+	    // if out of bounds, skip this racket
+	    if ( ( u1 < 0 ) || ( u1 > 1 ) || ( u2 < 0 ) || ( u2 > 1 ) ) {
+		// console.log("skip racket ( bounds ) " + ridx);
+		continue;
+	    }
+	    
+	    // haha, we got a collision !
+	    console.log("u1 = "  + u1 );
+	    console.log("u2 = " + u2 );
+	    console.log("collision with racket idx = " + ridx);
+	}	    
+
+	// collision with the map
+	// => distance from the center is greater than map ray
+	if ( dist_to_center > map_ray ) {
 	    this.direction += Math.PI;
 
-	    // fix position
-	    var dx = Math.cos( this.direction ) * ( dist - map_ray );
-	    var dy = Math.sin( this.direction ) * ( dist - map_ray );
+	    // FIXME: compute correctly the new position
+	    // FIXME that works only with perpendicular move
+	    var dx = Math.cos( this.direction ) * ( dist_to_center - map_ray );
+	    var dy = Math.sin( this.direction ) * ( dist_to_center - map_ray );
 
 	    this.x += dx;
 	    this.y += dy;
 	}
-
-
-	// do nothing
     };
 
     this.setIndex = function( idx ) {
@@ -58,15 +101,13 @@ function Ball( world, x, y ) {
     };
 
     this.initialize = function() {
-	// do nothing
+	// initial position
+	this.x = world.map.size / 2;
+	this.y = world.map.size / 2;
     }
 
     // constructor
     this.world = world;
-
-    // initial position
-    this.x = world.map.size / 2;
-    this.y = world.map.size / 2;
 
     // initial movement
     this.direction = Math.random() * 2 * Math.PI;
@@ -76,6 +117,12 @@ function Ball( world, x, y ) {
 
     // console.log( "Ball.new( x = " + this.x  + " ; y = " + this.y 
     //	+ " ; direction = " + this.direction + " ; speed = " + this.speed + " )");
+
+    // auto-generated values
+    this.x = null;
+    this.y = null;
+    this.dy = null;
+    this.dx = null;
 
     world.addBall( this );
 }
